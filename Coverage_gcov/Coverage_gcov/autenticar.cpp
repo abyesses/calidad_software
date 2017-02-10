4ef352019bf61d2795a02ab8cd42557001a06db2
Abraham Esses <abraham@Abrahams-MacBook-Pro.local>
2017-02-10 10:04:40 -0600

    OK

---
+++ Coverage_gcov/Coverage_gcov/autenticar.cpp
@@ -0,0 +1,9 @@
+//
+//  autenticar.cpp
+//  Coverage_gcov
+//
+//  Created by Abraham Esses on 2/7/17.
+//  Copyright © 2017 Abraham Esses. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Coverage_gcov/Coverage_gcov/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coverage_gcov
+//
+//  Created by Abraham Esses on 2/7/17.
+//  Copyright © 2017 Abraham Esses. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
